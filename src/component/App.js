df542d815508cd00e81eb09d63e0fddf086cba96
Kibuchi3560 <jontezbuchiki3560@gmail.com>
2024-11-14 17:19:48 +0300

    Updates

--- src/component/App.js
+++ src/component/App.js
@@ -1,17 +1,15 @@
 import React from 'react'
 import Footer from './Footer'
 import Details from './Details'
-import Header from './Header'
-import ChildCard from './ChildCard'
-import AddNewChild from './AddNewChild'
+
 
 export default function App() {
   return (
     <div>
-    <Header />
+    
     <Details />
-    <ChildCard />
-    <AddNewChild/>
+   
+    
     <Footer />
     </div>
   )
