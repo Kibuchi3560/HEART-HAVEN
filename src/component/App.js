a24afff7fdae1d90bf5e9845213656a3ee785034
Ian Kimutai <kimutaiian2@gmail.com>
2024-11-14 13:25:45 +0300

    add changes

--- src/component/App.js
+++ src/component/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import Header from './Header';
-import Footer from './Footer';
-import Details from './Details';
-import ChildList from './ChildList'; // Import ChildList
+import ChildList from './ChildList'; 
 
 export default function App() {
   return (
     <div>
-      <Header />
-      <Details />
-      <ChildList />  {/* Render ChildList here */}
-      <Footer />
+      
+      
+      <ChildList /> 
+    
     </div>
   );
 }
--- src/component/Header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Header() {
-  return (
-    <div className=''>
-        <h1>Heart Haven Adoption Services</h1>
-    </div>
-  )
-}
