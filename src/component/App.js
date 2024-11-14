91fd5fb471358025b9bf3790d614b9b542048a51
Kibuchi3560 <jontezbuchiki3560@gmail.com>
2024-11-14 14:17:56 +0300

    Updates App

---
+++ src/component/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Footer from './Footer'
+import Details from './Details'
+import Header from './Header'
+import ChildCard from './ChildCard'
+
+export default function App() {
+  return (
+    <div>
+    <Header />
+    <Details />
+    <ChildCard />
+    <Footer />
+    </div>
+  )
+}
